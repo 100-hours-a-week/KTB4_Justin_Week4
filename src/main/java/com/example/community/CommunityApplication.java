ab32c1188d0dcce22b96101bea04b402e41617c0
DDONG <shet6006@gmail.com>
2026-08-02 21:00:42 +0900

    chore: CI/CD 테스트용 변경사항 원복

--- src/main/java/com/example/community/CommunityApplication.java
+++ src/main/java/com/example/community/CommunityApplication.java
@@ -1,17 +1,13 @@
 package com.example.community;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CommunityApplication {
-	private static final Logger log = LoggerFactory.getLogger(CommunityApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(CommunityApplication.class, args);
-		log.info("TuneLog backend CI/CD deployment v1");
 	}
 
 }
